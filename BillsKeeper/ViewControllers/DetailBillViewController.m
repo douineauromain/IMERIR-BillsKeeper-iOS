880fd0b7621aa4095c3b777e303c82f9220f8757
DOUINEAU Romain <romain@toucouleur.fr>
2015-03-20 09:01:41 +0100

    share csv button
    ne fonctionne pas encore

--- BillsKeeper/ViewControllers/DetailBillViewController.m
+++ BillsKeeper/ViewControllers/DetailBillViewController.m
@@ -22,6 +22,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //configuration navigation bar :
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
+    
+    //autre
     [self.buttonCategory setTitle:@"Others" forState:UIControlStateNormal];
     
     NSLog(@"indexOfSelectedCellReceived : %d", [self.indexOfSelectedCellReceived intValue]);
@@ -29,9 +38,9 @@
     if ([self.fromShoot boolValue] != NO) { //from shoot
         [self.textFeildName becomeFirstResponder];
         
-        UITableViewCell *firstCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"firstCell"];
+        UITableViewCell *lastCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"lastCell"];
         
-        [self.tableView scrollToRowAtIndexPath:[self.tableView indexPathForCell:firstCell]
+        [self.tableView scrollToRowAtIndexPath:[self.tableView indexPathForCell:lastCell]
     atScrollPosition:UITableViewScrollPositionBottom
     animated:YES];
     }
@@ -68,7 +77,7 @@
 
 - (void) viewDidAppear:(BOOL)animated{
     //Bouton category
-    if ([[[NSUserDefaults standardUserDefaults] stringForKey:@"tempCategory"] isEqualToString:@""]) {
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"tempCategory"] == nil) {
         NSLog(@"Il n'y a pas de variable temporaire tempCategory");
     } else {
         NSLog(@"Il y a une variable temporaire tempCategory : %@",[[NSUserDefaults standardUserDefaults]stringForKey:@"tempCategory"]);
